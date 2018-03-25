9a2ebc8fef3eabab73d13f1ae07b0ac470f33552
jpc@wxyz.io Courtney <jpc@wxyz.io>
2018-03-25 02:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 }
+*/returns string al all caps/*
 
 function whisper(string) {
   return string.toLowerCase 
-  */returns/*
 }
+  */returns string as all lowercase/* 
